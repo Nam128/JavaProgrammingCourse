35dd0d9fbc750c7f25e23d713977604c74500f8f
Nam128 <76741571+Nam128@users.noreply.github.com>
2021-10-27 23:09:40 +0900

    week9-1
    Generic Programming

---
+++ JavaProgrammingClass/src/javaexample09/MyArray.java
@@ -0,0 +1,28 @@
+package javaexample09;
+
+public class MyArray <E> {
+    private int capacity;
+    private int count=0;
+    E[] arr;
+
+    @SuppressWarnings("unchecked")
+    public MyArray(int capacity) {
+        super();
+        this.capacity = capacity;
+        arr = (E[]) new Object[this.capacity]; // 타입파라미터로 배열을 생성할 수 없으므로 Object로 생성 후 강제 형변환.
+    }
+
+    public void add(E e) {
+        if(this.count < this.capacity) {
+            arr[count++] = e;
+        }
+        else
+            System.out.println("공간 부족");
+    }
+
+    public E getElement(int index) {
+        if(index >= 0 && index<count) {
+            return arr[index];
+        } else return null;
+    }
+}
